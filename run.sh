416fcc34b115066cbc28587586843a2ed794b613
Averill Cate, Jr <acatejr@gmail.com>
2024-03-18 16:33:44 -0700

    Added dotenv

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/sh
-docker run -v /home/acatejr/Documents/workspace/github.com/acatejr/nextgengeo:/home/jovyan/work -p 8888:8888 nextgengeo
+# docker run -v /home/acatejr/Documents/workspace/github.com/acatejr/nextgengeo:/home/jovyan/work -p 8888:8888 nextgengeo
+docker run -v $PWD:/home/jovyan/work -p 8888:8888 nextgengeo
